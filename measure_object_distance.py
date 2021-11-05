c8cd7c1f58cb29e485ad7c785db2307e336d6f5e
BgChina <67988865+BgChina@users.noreply.github.com>
2021-11-05 16:29:52 -0400

    Radically changed realsense camera to be more simple. Now detects distance off of mouse movement. Can create pointcloud object with 3D map.

--- measure_object_distance.py
+++ measure_object_distance.py
@@ -1,36 +1,34 @@
-#https://pysource.com
 import cv2
+import pyrealsense2
+#import pointcloud as pc
 from realsense_camera import *
-from mask_rcnn import *
 
+point = (400, 300)
+#pc = pointcloud();
+def show_distance(event, x, y, args, params):
+    global point
+    point = (x, y)
 
-# Load Realsense camera
-#rs = RealsenseCamera()
-mrcnn = MaskRCNN()
+# Initialize Camera Intel Realsense
+dc = DepthCamera()
+
+# Create mouse event
+cv2.namedWindow("Color frame")
+cv2.setMouseCallback("Color frame", show_distance)
 
 while True:
-	# Get frame in real time from Realsense camera
-	#ret, bgr_frame, depth_frame = rs.get_frame_stream()
+    ret, depth_frame, color_frame = dc.get_frame()
 
-	# Get object mask
-	#boxes, classes, contours, centers = mrcnn.detect_objects_mask(bgr_frame)
+    # Show distance for a specific point
+    cv2.circle(color_frame, point, 4, (0, 0, 255))
+    distance = depth_frame[point[1], point[0]]
 
-	# Draw object mask
-	#bgr_frame = mrcnn.draw_object_mask(bgr_frame)
+    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
-	# Show depth info of the objects
-	#mrcnn.draw_object_info(bgr_frame, depth_frame)
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+    key = cv2.waitKey(1)
 
-	#get QR code
-	mrcnn.imDetect();
-
-	#cv2.imshow("depth frame", depth_frame)
-	#cv2.imshow("Bgr frame", bgr_frame)
-
-	key = cv2.waitKey(1)
-	if key == 27:
-		break
-
-rs.release()
-cv2.destroyAllWindows()
-outputFile.close
+    #
+    if key == 27:
+        break
